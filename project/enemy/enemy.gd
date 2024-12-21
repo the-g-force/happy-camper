ae5fc7b108d2daa7d99a607e2e1e986c26d1cdc3
Alex G <alex.gestwicki@gmail.com>
2024-12-21 10:13:51 -0500

    make enemy do damage to player

--- project/enemy/enemy.gd
+++ project/enemy/enemy.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 @export var target : Player
-@export var speed := 300
+@export var speed := 200
+
 
 func _physics_process(delta: float) -> void:
 	var angle := global_position.angle_to_point(target.global_position)
 	move_and_collide(Vector2.RIGHT.rotated(angle) * speed * delta)
+
+
+func _on_hit_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.damage()
--- project/player/player.gd
+++ project/player/player.gd
@@ -3,7 +3,16 @@ extends CharacterBody2D
 
 @export var speed := 300
 
+var health := 10
+
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down").normalized()
 	move_and_collide(direction * speed * delta)
+
+
+func damage() -> void:
+	health -= 1
+	print(health)
+	if health <= 0:
+		print("you died")
