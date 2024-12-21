1a2019d35851780da245be56c6591eeef6b40b5f
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-12-21 11:03:59 -0500

    Play animation and change player facing

--- project/player/player.gd
+++ project/player/player.gd
@@ -1,14 +1,25 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed := 300
+@export var speed := 100
 
 var health := 10
 
+@onready var _sprite := $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down").normalized()
-	move_and_collide(direction * speed * delta)
+	if direction.is_zero_approx():
+		_sprite.stop()
+	else:
+		_sprite.play("default")
+	if direction.x < 0:
+		_sprite.flip_h = true
+	elif direction.x > 0:
+		_sprite.flip_h = false
+	
+	var motion := direction * speed
+	move_and_collide(motion * delta)
 
 
 func damage() -> void:
@@ -16,3 +27,7 @@ func damage() -> void:
 	print(health)
 	if health <= 0:
 		print("you died")
+
+
+func is_facing_right() -> bool:
+	return _sprite.flip_h == false
