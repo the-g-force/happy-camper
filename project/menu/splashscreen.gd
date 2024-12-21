28a26d3f04047728677f5a362c7a67c73a73deb2
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-12-21 17:19:06 -0500

    Make sure it is a mouse event
    It could previously also have been a keyboard event

--- project/menu/splashscreen.gd
+++ project/menu/splashscreen.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_pressed():
+	if event is InputEventMouseButton and event.is_pressed():
 		get_tree().change_scene_to_file("res://menu/main_menu.tscn")
