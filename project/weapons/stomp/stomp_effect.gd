5bbcc501345ccca39e6abc69798a054e1fb7d275
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-12-21 12:32:05 -0500

    Add stomp weapon

---
+++ project/weapons/stomp/stomp_effect.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		body.damage(1)
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	queue_free()
---
+++ project/weapons/stomp/stomp_weapon.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var min_range := 0
+@export var max_range := 32 * 6
+
+func _on_timer_timeout() -> void:
+	var stomp := preload("res://weapons/stomp/stomp_effect.tscn").instantiate()
+	get_parent().add_child(stomp)
+	var distance := randf_range(min_range, max_range)
+	var angle := randf_range(0,TAU)
+	stomp.global_position = global_position + Vector2(distance,0).rotated(angle)
