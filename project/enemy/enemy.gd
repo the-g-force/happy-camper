fb9271e7f8a231fc864d5dbe91fada68f5840cca
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-12-21 10:54:30 -0500

    Make flyswatter hit enemies

--- project/enemy/enemy.gd
+++ project/enemy/enemy.gd
@@ -1,7 +1,9 @@
-extends CharacterBody2D
+class_name Enemy extends CharacterBody2D
 
 @export var target : Player
 @export var speed := 200
+
+var _health := 1.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,3 +14,11 @@ func _physics_process(delta: float) -> void:
 func _on_hit_area_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.damage()
+
+
+## Apply this much damage to the enemy.
+## If it takes more damage than its health, it dies.
+func damage(amount:int) -> void:
+	_health -= amount
+	if _health <= 0:
+		queue_free()
--- project/weapons/flyswatter.gd
+++ project/weapons/flyswatter.gd
@@ -1,20 +1,32 @@
 extends Node2D
 
 @onready var _area := $Area2D
+@onready var _collision := $Area2D/CollisionShape2D
 
 func _ready() -> void:
+	_deactivate()
+	
+
+func _activate() -> void:
+	_area.visible = true
+	_collision.disabled = false
+
+
+func _deactivate() -> void:
 	_area.visible = false
-
-
-func _on_timer_timeout() -> void:
-	_area.visible = not _area.visible
+	_collision.disabled = true
 
 
 func _on_active_timer_timeout() -> void:
-	_area.visible = false
+	_deactivate()
 	$WaitTimer.start()
 
 
 func _on_wait_timer_timeout() -> void:
-	_area.visible = true
+	_activate()
 	$ActiveTimer.start()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		body.damage(1)
